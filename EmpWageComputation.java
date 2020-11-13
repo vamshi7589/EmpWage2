a7dcea91f41ecf165ac0c1a763c29f10806b7d86
vamshi7589 <vamshi7589@gmail.com>
2020-11-13 07:30:41 +0530

    added: added employee wage calculation program

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,18 +1,35 @@
+public class EmployeeWageComputation
 {
-        public static void main(String[] args) {
-                System.out.println("Welcome to Employee Wage Computation");
-                final int wagePerHour=20;
-                final int fullDayHour=8;
-                final int partTimeHour=4;
-                int dailyWage=0;
-                final int isFullTime=1;
-                final int isPartTime=2;
-                int empCheck = (int)Math.floor(Math.random()*10)%3;
-                switch (empCheck) {
-                case isFullTime:
-                        System.out.println("Employee is full time");
-                        dailyWage=(wagePerHour*fullDayHour);
-                        System.out.println("daily wage is "+dailyWage);
-                        break;
-                case isPartTime:
-                        System.out.println("Employee is part t
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation");
+		final int wagePerHour=20;
+		final int fullDayHour=8;
+		final int partTimeHour=4;
+		int dailyWage=0;
+		final int isFullTime=1;
+		final int isPartTime=2;
+		int days=0;
+		int monthlyWage=0;
+		int currentWorkingHours=0;
+		final int maxWorkingHours=100;
+		final int totMonthDays=20;
+		while( currentWorkingHours<=maxWorkingHours || days<=totMonthDays) {
+			int empCheck = (int)Math.floor(Math.random()*10)%3;
+			days++;
+			switch (empCheck) {
+			case isFullTime:
+				dailyWage=(wagePerHour*fullDayHour);
+				currentWorkingHours+=fullDayHour;
+				break;
+			case isPartTime:
+				dailyWage=(wagePerHour*partTimeHour);
+				currentWorkingHours+=partTimeHour;
+				break;
+			default:
+				break;
+			}
+			monthlyWage+=dailyWage;
+		}
+		System.out.println("Total monthly salary is "+monthlyWage);
+	}
+}
